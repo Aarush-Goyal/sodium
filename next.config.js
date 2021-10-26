a73792e560a9e3cf4dbb8ccae87f2805d0a465b7
Aarush Goyal <64161383+Aarush-Goyal@users.noreply.github.com>
2021-10-26 13:40:46 +0530

    feat: added community link in `/community` route
    Now the users will be redirected to the discord community when they go on https://jobunicorn.io/community

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
 				destination: 'https://www.youtube.com/watch?v=s7mozY3j4Nw',
 				permanent: true,
 			},
+			{
+				source: '/community',
+				destination: 'https://discord.gg/MJaHr8V3Ar',
+				permanent: true,
+			}
 		]
 	},
 }
